78b9198ae860420ccf4fb1d877ab5199b8e20f63
robin.limit <robin.limit@outlook.com>
2023-09-29 21:06:42 +0800

    完善包管理案例

--- mod_crates/src/demo_mod/mod.rs
+++ mod_crates/src/demo_mod/mod.rs
@@ -2,6 +2,10 @@
     文件夹名称就是mod名称，只要文件夹中新建了mod.rs文件。
     下一级的mod，可以在此文件中“注册”
 */
+
+//下一级mod在此注册
+pub mod sub;
+
 pub fn mod_name() {
     println!("mod name: demo_mod")
 }
--- mod_crates/src/main.rs
+++ mod_crates/src/main.rs
@@ -1,11 +1,15 @@
 //使用use关键字在根节点引入mod
-use mod_crates::demo_mod::*;
+use mod_crates::demo_mod::{mod_name, sub};
 //use关键字引入类库
 use pub_lib::add;
+use sub::sub;
 
 fn main() {
-    let result = add(1, 2);
-    println!("1 + 2 = {}", result);
-    //在mod中定义的pub方法
-    mod_name()
+    let add_result = add(1, 2);
+    println!("1 + 2 = {}", add_result);
+    //使用在子模块demo_mod中定义的pub方法
+    mod_name();
+    //使用在子模块demo_mod的子模块sub中定义的方法
+    let sub_result = sub(-1, -2);
+    println!("-1-(-2) = {}", sub_result)
 }
