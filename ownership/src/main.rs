3d4a580e0a5d18cb9bd389eb40a8e93a49bf24e6
robin.limit <robin.limit@outlook.com>
2023-10-02 23:14:01 +0800

    内存分配

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -24,9 +24,9 @@ fn change_value(arg: i32) -> i32 {
 //参数直接使用String类型
 fn count_character(str: String) -> usize {
     str.len()
-}
+} //离开函数作用域,str变量失效,堆内存上的数据也随之销毁
 
 // 参数使用&str引用类型
 fn count_char_by_ref(str: &str) -> usize {
     str.len()
-}
+} //离开函数作用域,str变量失效,但是由于参数传入过程没有发生所有权转移,所以堆内存上的值没有被销毁
